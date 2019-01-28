ee32cba6dc9a2cd5be9bd913ad7b1390eef304b1
kmuranga <kmuranga@retrorbbit>
2019-01-28 13:37:59 +0200

    Changed dummy branch to dev

---
+++ Dummy/Dummy/Models/DummyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dummy.Models
+{
+    public class DummyContext
+    {
+        public DummyContext(DbContextOptions<DummyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DummyModel> TodoItems { get; set; }
+    }
+}
