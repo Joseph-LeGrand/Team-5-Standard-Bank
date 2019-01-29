fc208f4f510d68049305077f60973a3295d8ab4f
Xolani-95 <xdlamini@retrorabbit.co.za>
2019-01-29 22:33:21 +0200

    "should be fine"

---
+++ Dummy.test/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Dummy.Controllers;
+using Dummy.Models;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private DummyModel _context;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+
+          /*  //Arrange
+            var testLogin = new DummyModelsController();
+            //Act
+         
+            //Assert*/
+        }
+    }
+}
