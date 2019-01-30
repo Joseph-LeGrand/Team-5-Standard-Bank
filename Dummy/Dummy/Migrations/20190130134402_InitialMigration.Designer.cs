e0362c5f118b1adef400e8db93003d63d78c5d04
kmuranga <kmuranga@retrorbbit>
2019-01-30 15:54:46 +0200

    Fixed errors

--- Dummy/Dummy/Migrations/20190130134402_InitialMigration.Designer.cs
+++ Dummy/Dummy/Migrations/20190130134402_InitialMigration.Designer.cs
@@ -3,14 +3,16 @@ using Dummy.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dummy.Migrations
 {
     [DbContext(typeof(DummyContext))]
-    partial class DummyContextModelSnapshot : ModelSnapshot
+    [Migration("20190130134402_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
