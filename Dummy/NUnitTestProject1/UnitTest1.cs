c074dcb742746c6d2e973f9dcb35ee1566dc430f
Mpinane <mmohale@retrorabbit.co.za>
2019-02-04 10:21:49 +0200

    Fixed login tests

--- Dummy/NUnitTestProject1/UnitTest1.cs
+++ Dummy/NUnitTestProject1/UnitTest1.cs
@@ -66,110 +66,56 @@ namespace Tests
 
         }
 
-        [Test]
-        public void Login()
+        [TestCase("mpi", "nane")]
+        public void LoginPass(string username, string password)
         {
             //Arrange
+            string salt = Salt.Create();
+            IQueryable<DummyModel> mockData = new List<DummyModel> {
+               new DummyModel {Id = 1, FirstName = "mpinane", LastName = "mohale", Username = "mpi", Password = Hash.Create("nane", salt) , Salt = salt}
+            }.AsQueryable();
+
+            var mockSet = new Moq.Mock<DbSet<DummyModel>>();
+            mockSet.Setup(m => m.Find(username)).Returns((mockData.Where(x => x.Username == username)).First);
+
+            var mockContext = new Moq.Mock<DummyContext>();
+            mockContext.Setup(c => c.DummyModel).Returns(mockSet.Object);
+
+            var controller = new DummyModelsController(mockContext.Object);
+
 
             //Act
+            var login = controller.Login(username, password).Result;
+
 
             //Assert
-            Assert.Pass();
+            Assert.IsInstanceOf<OkObjectResult>(login);
         }
 
+        [TestCase("mpi", "nana")]
+        public void LoginFail(string username, string password)
+        {
+            //Arrange
+            string salt = Salt.Create();
+            IQueryable<DummyModel> mockData = new List<DummyModel> {
+               new DummyModel {Id = 1, FirstName = "mpinane", LastName = "mohale", Username = "mpi", Password = Hash.Create("nane", salt) , Salt = salt}
+            }.AsQueryable();
 
-        /* 
-            /*   var dummyModel = new DummyModel();
-               dummyModel.FirstName = "Mp";
-               dummyModel.LastName = "Mohale";
-               dummyModel.Password = "nane";
-               dummyModel.Username = "mpi";
-           //    string salt = Salt.Create();
-             //  string passwordHash = Hash.Create(dummyModel.Password, salt);
-               dummyModel.Password = passwordHash;
-               dummyModel.Salt = salt;
-      
-         * [Test]
-           public async Task CreateBlog_saves_a_blog_via_contextAsync()
-           {
-               var context = new Moq.Mock<DummyContext>();
-               DummyModelsController dummyController = new DummyModelsController(context.Object);
+            var mockSet = new Moq.Mock<DbSet<DummyModel>>();
+            mockSet.Setup(m => m.Find(username)).Returns((mockData.Where(x => x.Username == username)).First);
 
+            var mockContext = new Moq.Mock<DummyContext>();
+            mockContext.Setup(c => c.DummyModel).Returns(mockSet.Object);
 
-               var mockSet = new Moq.Mock<DbSet<DummyModel>>();
-
-               var mockContext = new Moq.Mock<DummyContext>();
-               mockContext.Setup(m => m.DummyModel).Returns(mockSet.Object);
-
-               var controller = new DummyModelsController(mockContext.Object);
-               await controller.Register(new (neeMoq.Mock<DummyModel> { FirstName = "Mpinane", LastName = "Mohale", Username = "mpi", Password = "nane" });
-
-               int x = 0;
-               mockSet.Verify(m => m.Add(It.IsAny<DummyModel>()), Times.Once());
-               mockContext.Verify(m => m.SaveChanges(), Times.Once());
-           }
-           */
+            var controller = new DummyModelsController(mockContext.Object);
 
 
+            //Act
+            var login = controller.Login(username, password).Result;
 
 
-
-
-
-
-
-        /*   HttpClient _client = new HttpClient();
-          HttpListener _httpListener = new HttpListener();
-
-          [Test]
-          public async Task RegisterTest()
-          {
-
-              //Arrange
-
-              var dummyModel = new DummyModel { FirstName = "Mpinane", LastName = "Mohale", Username = "mpi", Password = "nane" };
-
-              var stringContent = new StringContent(JsonConvert.SerializeObject(dummyModel));
-
-              _httpListener.Prefixes.Add("http://localhost:44312/");
-              _httpListener.Start();
-
-              //Act
-              var response = await _client.PostAsync("http://localhost:44312/users/register", stringContent);
-             // _httpListener.Stop();
-              var x = response;
-
-              // Check if status code is OK
-              Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-              // Get JSON  of the dummyModel from response
-              var jsonResponse = await response.Content.ReadAsStringAsync();
-              // Deserialize response JSON to Empoyee class
-              var EmployeeResponse = JsonConvert.DeserializeObject<DummyModel>(jsonResponse);
-              // Check if the Employee is the same
-
-              Assert.AreEqual(dummyModel.FirstName, EmployeeResponse.FirstName);
-              Assert.AreEqual(dummyModel.LastName, EmployeeResponse.LastName);
-              Assert.AreEqual(dummyModel.Username, EmployeeResponse.Username);
-              Assert.AreEqual(dummyModel.Password, EmployeeResponse.Password);
-              */
-
-        //private DummyContext _context = ;
-
-        /*   Mock<DummyContext> dummyContext = new Mock<DummyContext>();
-
-                [SetUp]
-                public void Setup()
-                {
-
-                }
-
-
-                */
-
-
-
-
-
-
+            //Assert
+            Assert.IsInstanceOf<UnauthorizedResult>(login);
+        }
     }
 }
